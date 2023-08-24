83d8f9538d227b5bc87556ce10af28fb2d765322
Abdulrahmon6764 <lacymia679@gmail.com>
2023-08-24 06:32:42 -0700

    change

---
+++ gart_drag/src/assets/images/index.jsx
@@ -0,0 +1,6 @@
+import logo from "./logo.jpg";
+import racing from "./racing.jpg";
+import buggati from "./buggati.png";
+import racingBanner from "./racing-banner.jpg";
+
+export { logo, racing, buggati, racingBanner };
